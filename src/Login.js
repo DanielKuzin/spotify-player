1b3e09a35df9e924269e5efb96d399baa9f67316
Daniel Kuzin <43601854+DanielKuzin@users.noreply.github.com>
2022-05-16 20:46:02 +0300

    fix

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 const auth_url = "https://accounts.spotify.com/authorize";
 const client_id = "1b92d625825c465aa3f2ec73e22162b5";
 const response_type = "code";
-const redirect_uri = "http://localhost:3000";
+const redirect_uri = "https://dans-player.netlify.app/";
 const scope =
   "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state";
 
